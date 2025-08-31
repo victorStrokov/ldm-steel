e184f7132f8c2f1b12a9ef1e44e6fc99eb9b6479
Victor <144254586+victorStrokov@users.noreply.github.com>
2025-08-31 21:53:32 +0300

    feat(seed): update Prisma schema, constants, and seed script; add assets

--- prisma/constants.ts
+++ prisma/constants.ts
@@ -1,221 +1,402 @@
 export const categories = [
-  { name: "Армирование" },
-  { name: "Алюминий" },
-  { name: "ПВХ" },
-  { name: "Уплотнение" },
-  { name: "Фурнитура" },
-  { name: "Комплектующие" },
+  { name: 'Армирование' },
+  { name: 'Алюминий' },
+  { name: 'ПВХ' },
+  { name: 'Уплотнение' },
+  { name: 'Фурнитура' },
+  { name: 'Комплектующие' },
 ];
 
 export const ingredients = [
   {
-    name: "Соединитель механический импоста Montblanc Eco 60",
+    name: 'Соединитель механический импоста Montblanc Eco 60',
     price: 1500,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=fb8cc41a6288f70cc70ae82cc16d9c93c535f8f5-5214146-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=fb8cc41a6288f70cc70ae82cc16d9c93c535f8f5-5214146-images-thumbs&n=13',
   },
   {
-    name: "Саморезы оконные SG 3,9*16 белые плоские Phillips",
+    name: 'Саморезы оконные SG 3,9*16 белые плоские Phillips',
     price: 100,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=a35d0bca683f2a9cd2c72f2be9cc79c6e4f44abf-8487401-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=a35d0bca683f2a9cd2c72f2be9cc79c6e4f44abf-8487401-images-thumbs&n=13',
   },
   {
-    name: "Саморезы оконные SG 3,9*19 белые плоские Phillips",
+    name: 'Саморезы оконные SG 3,9*19 белые плоские Phillips',
     price: 100,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=a35d0bca683f2a9cd2c72f2be9cc79c6e4f44abf-8487401-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=a35d0bca683f2a9cd2c72f2be9cc79c6e4f44abf-8487401-images-thumbs&n=13',
   },
   {
-    name: "Саморез для импоста 6,3*70",
+    name: 'Саморез для импоста 6,3*70',
     price: 160,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=e322c16d31c97d80ae1889e0ebd60e732608f959-4231391-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=e322c16d31c97d80ae1889e0ebd60e732608f959-4231391-images-thumbs&n=13',
   },
   {
-    name: "Саморез с прессшайбой острый 4,2*38",
+    name: 'Саморез с прессшайбой острый 4,2*38',
     price: 100,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=08360bdc63f11c267b05c8745601f447037f23d2-5239800-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=08360bdc63f11c267b05c8745601f447037f23d2-5239800-images-thumbs&n=13',
   },
   {
-    name: "Держатель магнитный",
+    name: 'Держатель магнитный',
     price: 100,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=564e6142cb39f7a899c9a81d3533f944946458f9-4628144-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=564e6142cb39f7a899c9a81d3533f944946458f9-4628144-images-thumbs&n=13',
   },
   {
-    name: "Ручка для москитной сетки",
+    name: 'Ручка для москитной сетки',
     price: 100,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=0e2c79d5b6c2a54c0c5a00bd10eae99674bb3c14-4576389-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=0e2c79d5b6c2a54c0c5a00bd10eae99674bb3c14-4576389-images-thumbs&n=13',
   },
   {
-    name: "Штифт пружинный с капсулой",
+    name: 'Штифт пружинный с капсулой',
     price: 145,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=328732424f4a351e324fbd5a4087be307db4845e-16444048-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=328732424f4a351e324fbd5a4087be307db4845e-16444048-images-thumbs&n=13',
   },
   {
-    name: "Шнур ПВХ",
+    name: 'Шнур ПВХ',
     price: 110,
     imageUrl:
-      "http://ldm-steel.com/wp-content/uploads/image/moskitos/shnurpvc.jpg",
+      'http://ldm-steel.com/wp-content/uploads/image/moskitos/shnurpvc.jpg',
   },
   {
-    name: "Крепеж длинный",
+    name: 'Крепеж длинный',
     price: 79,
     imageUrl:
-      "http://ldm-steel.com/wp-content/uploads/image/moskitos/kr_dl_m.png",
+      'http://ldm-steel.com/wp-content/uploads/image/moskitos/kr_dl_m.png',
   },
   {
-    name: "Крепеж короткий",
+    name: 'Крепеж короткий',
     price: 79,
     imageUrl:
-      "http://ldm-steel.com/wp-content/uploads/image/moskitos/kr_kr_m.png",
+      'http://ldm-steel.com/wp-content/uploads/image/moskitos/kr_kr_m.png',
   },
   {
-    name: "Сетка москитная",
+    name: 'Сетка москитная',
     price: 79,
     imageUrl:
-      "http://ldm-steel.com/wp-content/uploads/image/moskitos/setka_rul.jpg",
+      'http://ldm-steel.com/wp-content/uploads/image/moskitos/setka_rul.jpg',
   },
   {
-    name: "Уголок для маскитной сетки серый",
+    name: 'Уголок для маскитной сетки серый',
     price: 79,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=939e791ca531d61d4693204120d87030f1a1b7f7-5521840-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=939e791ca531d61d4693204120d87030f1a1b7f7-5521840-images-thumbs&n=13',
   },
   {
-    name: "Уголок для маскитной сетки белый",
+    name: 'Уголок для маскитной сетки белый',
     price: 79,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=2a5fccb9faf458c597c007c7322da0bcbbf35d55-6291274-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=2a5fccb9faf458c597c007c7322da0bcbbf35d55-6291274-images-thumbs&n=13',
   },
   {
-    name: "Уголок для маскитной сетки коричневый",
+    name: 'Уголок для маскитной сетки коричневый',
     price: 79,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=9b8809a6f01e6186d1d9e381f072240bebe887c3-10893744-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=9b8809a6f01e6186d1d9e381f072240bebe887c3-10893744-images-thumbs&n=13',
   },
   {
-    name: "крепление для москитной сетки коричневое",
+    name: 'крепление для москитной сетки коричневое',
     price: 79,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=d7ffbcd72cdf82e0a354d124bb24dc62894c018d-16330514-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=d7ffbcd72cdf82e0a354d124bb24dc62894c018d-16330514-images-thumbs&n=13',
   },
   {
-    name: "Крепления для москитной сетки белые",
+    name: 'Крепления для москитной сетки белые',
     price: 79,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=528ef5ba2b38de79f2c747396b9367378fc2b5a8-5167369-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=528ef5ba2b38de79f2c747396b9367378fc2b5a8-5167369-images-thumbs&n=13',
   },
   {
-    name: "Крепление поперечного профиля москитной сетки",
+    name: 'Крепление поперечного профиля москитной сетки',
     price: 79,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=26c617aac13425b9a908d774c7562a19cd3875d1-5221624-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=26c617aac13425b9a908d774c7562a19cd3875d1-5221624-images-thumbs&n=13',
   },
   {
-    name: "Ручка малая для москитной сетки коричневая",
+    name: 'Ручка малая для москитной сетки коричневая',
     price: 79,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=64208cf796d1d1ec0ccbc0696ded998541fec41c-16509553-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=64208cf796d1d1ec0ccbc0696ded998541fec41c-16509553-images-thumbs&n=13',
   },
   {
-    name: "Ручка для москитной сетки металлическая белая",
+    name: 'Ручка для москитной сетки металлическая белая',
     price: 79,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=b2127432ffca158ade9adebe7817a04bcbe4b04e15c28041-11492537-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=b2127432ffca158ade9adebe7817a04bcbe4b04e15c28041-11492537-images-thumbs&n=13',
   },
   {
-    name: "Петля для москитной сетки коричневая",
+    name: 'Петля для москитной сетки коричневая',
     price: 79,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=8a15ea3d50514247ef8da8769e1687e3c416c164-3725564-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=8a15ea3d50514247ef8da8769e1687e3c416c164-3725564-images-thumbs&n=13',
   },
   {
-    name: "Петля для москитной сетки белая",
+    name: 'Петля для москитной сетки белая',
     price: 79,
     imageUrl:
-      "https://avatars.mds.yandex.net/i?id=81bc11268994b2db9fa6e8e6ecdd844599c98e58-5288655-images-thumbs&n=13",
+      'https://avatars.mds.yandex.net/i?id=81bc11268994b2db9fa6e8e6ecdd844599c98e58-5288655-images-thumbs&n=13',
   },
 ].map((obj, index) => ({ id: index + 1, ...obj }));
 
 export const products = [
   {
-    name: "",
-    imageUrl: "",
+    name: 'Труба 40х50х2мм с вырубкой под замок (2,2м)',
+    imageUrl: 'public/assets/труба-с-вырубкой.png',
+    price: 1500,
     categoryId: 1,
   },
   {
-    name: "",
-    imageUrl: "",
+    name: 'Труба сварная 40х50х2мм (6м)',
+    imageUrl: 'public/assets/труба-сварная-40х50х2мм-6м.jpg',
+    price: 1500,
     categoryId: 1,
   },
   {
-    name: "",
-    imageUrl: "",
+    name: 'Профиль армирующий 35х42х2мм (6м)',
+    imageUrl: 'public/assets/профиль-армирующий-35х42х2мм-6м.jpg',
+    price: 1500,
     categoryId: 1,
   },
   {
-    name: "",
-    imageUrl: "",
+    name: 'Полоса оцинкованная 100х6мм (6м)',
+    imageUrl: 'public/assets/Полоса оцинкованная 100х6мм (6м).jpg',
+    price: 1500,
     categoryId: 1,
   },
   {
-    name: "",
-    imageUrl: "",
+    name: 'REHAU 245536',
+    imageUrl: 'public/assets/REHAU 245536.png',
+    price: 1500,
     categoryId: 1,
   },
   {
-    name: "",
-    imageUrl: "",
+    name: 'REHAU 244506',
+    imageUrl: 'public/assets/REHAU 244506.jpg',
+    price: 1500,
     categoryId: 1,
   },
   {
-    name: "",
-    imageUrl: "",
+    name: 'Подкладка рихтовочная 100х18х1',
+    imageUrl: 'public/assets/Подкладка рихтовочная.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Подкладка рихтовочная 100х18х6',
+    imageUrl: 'public/assets/Подкладка рихтовочная.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Подкладка рихтовочная 100х24х1',
+    imageUrl: 'public/assets/Подкладка рихтовочная.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Подкладка рихтовочная 100х24х6',
+    imageUrl: 'public/assets/Подкладка рихтовочная.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Подкладка рихтовочная 100х28х6',
+    imageUrl: 'public/assets/Подкладка рихтовочная.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Подкладка рихтовочная 100х30х6',
+    imageUrl: 'public/assets/Подкладка рихтовочная.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Подкладка рихтовочная 100х34х6',
+    imageUrl: 'public/assets/Подкладка рихтовочная.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Подкладка рихтовочная 100х40х6',
+    imageUrl: 'public/assets/Подкладка рихтовочная.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Фальцевый вкладыш 17.166 Montblanc',
+    imageUrl: 'public/assets/Фальцевый вкладыш 17.166 Montblanc.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Фальцевый вкладыш 17.165 Montblanc',
+    imageUrl: 'public/assets/Фальцевый вкладыш 17.165 Montblanc.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Механический соединитель импоста Rehau Basic',
+    imageUrl: 'public/assets/Механический соединитель импоста Rehau Basic.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Механический соединитель импоста Renau Basic M (248656)',
+    imageUrl:
+      'public/assets/Механический соединитель импоста Renau Basic M (248656).jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Штульповая заглушка 1780 (Rehau)',
+    imageUrl: 'public/assets/Штульповая заглушка 1780 (Rehau).jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Ручка оконная с ключом',
+    imageUrl: 'public/assets/Ручка оконная с ключом.jpg',
+    price: 1540,
+    categoryId: 5,
+  },
+  {
+    name: 'Клин монтажный 115х30х16',
+    imageUrl: 'public/assets/Клин монтажный 115х30х16.jpg',
+    price: 150,
+    categoryId: 6,
+  },
+  {
+    name: 'Ручка Hoppe (алюминиевая, 8-ми позиционная, штифт 35, 40 мм, белая, золото, корич, серебро)',
+    imageUrl: 'public/assets/Ручка Hoppe.jpg',
+    price: 1990,
+    categoryId: 5,
+  },
+  {
+    name: 'ПВХ профиль REACHMONT Рама 60мм',
+    imageUrl: 'public/assets/ПВХ профиль REACHMONT Рама 60мм.jpg',
+    price: 1500,
+    categoryId: 3,
+  },
+  {
+    name: 'ПВХ профиль REACHMONT Створка 60мм',
+    imageUrl: 'public/assets/REACHMONT Створка 60мм.jpg',
+    price: 1500,
+    categoryId: 3,
+  },
+  {
+    name: 'ПВХ профиль REACHMONT Импост 60мм',
+    imageUrl: 'public/assets/REACHMONT Импост 60мм.jpg',
+    price: 1500,
+    categoryId: 3,
+  },
+  {
+    name: 'ПВХ профиль REACHMONT Профиль Соединительный 60мм',
+    imageUrl: 'public/assets/REACHMONT Профиль Соединительный 60мм.jpg',
+    price: 1500,
+    categoryId: 3,
+  },
+  {
+    name: 'Рама Нижняя Provedal КПС 034',
+    imageUrl: 'public/assets/Рама Нижняя Provedal КПС 034.jpg',
+    price: 1500,
+    categoryId: 2,
+  },
+  {
+    name: 'Provedal Рама Верхняя (КПС 035)',
+    imageUrl: 'public/assets/Provedal Рама Верхняя (КПС 035).jpg ',
+    price: 2150,
+    categoryId: 2,
+  },
+  {
+    name: 'Provedal Рама Боковая (КПС 036)',
+    imageUrl: 'public/assets/Provedal Рама Боковая (КПС 036).jpg',
+    price: 2150,
+    categoryId: 2,
+  },
+  {
+    name: 'Рама Верхняя (арт. Provedal С640/01)',
+    imageUrl: 'public/assets/Рама Верхняя.jpg',
+    price: 2150,
+    categoryId: 2,
+  },
+  {
+    name: 'Створка Provedal Верх-Низ С64012',
+    imageUrl: 'public/assets/Створка Верх-Низ С64012.jpg',
+    price: 2150,
     categoryId: 1,
   },
   {
-    name: "",
-    imageUrl: "",
+    name: 'Створка Боковая Provedal С64010',
+    imageUrl: 'public/assets/Створка Боковая Provedal С64010.jpg',
+    price: 2150,
     categoryId: 1,
   },
   {
-    name: "",
-    imageUrl: "",
-    categoryId: 1,
+    name: 'Уплотнитель Gealan 2149',
+    imageUrl: 'public/assets/Уплотнитель Gealan 2149.jpg',
+    price: 3150,
+    categoryId: 4,
   },
   {
-    name: "",
-    imageUrl: "",
-    categoryId: 1,
+    name: 'Уплотнитель Rehau 864952',
+    imageUrl: 'public/assets/Уплотнитель Rehau 864952.jpg',
+    price: 1150,
+    categoryId: 4,
   },
   {
-    name: "",
-    imageUrl: "",
-    categoryId: 1,
+    name: 'Уплотнитель Veka 253 (черн, серая)',
+    imageUrl: 'public/assets/Уплотнитель Veka 253 (черн, серая).jpg',
+    price: 1950,
+    categoryId: 4,
   },
   {
-    name: "",
-    imageUrl: "",
-    categoryId: 1,
+    name: 'Уплотнитель KBE 227',
+    imageUrl: 'public/assets/Уплотнитель KBE 227.jpg',
+    price: 1150,
+    categoryId: 4,
   },
   {
-    name: "",
-    imageUrl: "",
-    categoryId: 1,
+    name: 'Уплотнитель KBE 255',
+    imageUrl: 'public/assets/Уплотнитель KBE 255.jpg',
+    price: 2150,
+    categoryId: 4,
   },
   {
-    name: "",
-    imageUrl: "",
-    categoryId: 1,
+    name: 'Наружный прижим с накладкой',
+    imageUrl: 'public/assets/Наружный прижим с накладкой.jpg',
+    price: 215,
+    categoryId: 5,
   },
   {
-    name: "",
-    imageUrl: "",
-    categoryId: 1,
+    name: 'Скрытый прижим (регулируемый)',
+    imageUrl: 'public/assets/Скрытый прижим (регулируемый).jpg',
+    price: 150,
+    categoryId: 5,
+  },
+  {
+    name: 'Комплект (Петля верхняя+штифт+декор накладки) аналог Siegenia',
+    imageUrl:
+      'public/assets/Комплект (Петля верхняя+штифт+декор накладки) аналог Siegenia.jpg',
+    price: 1588,
+    categoryId: 5,
+  },
+  {
+    name: 'Комплект (Петля верхняя+штифт+декор накладки) аналог Roto NT',
+    imageUrl:
+      'public/assets/Комплект (Петля верхняя+штифт+декор накладки) аналог Roto NT.jpg',
+    price: 750,
+    categoryId: 5,
+  },
+  {
+    name: 'Комплект (Петля нижняя рама+Петля нижняя створка+декор накладки) аналог Roto NT',
+    imageUrl:
+      'public/assets/Комплект (Петля нижняя рама+Петля нижняя створка+декор накладки) аналог Roto NT.jpg',
+    price: 810,
+    categoryId: 5,
   },
 ];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,61 @@
-import { hashSync } from "bcrypt";
-import { prisma } from "./prisma-client";
-import { categories, ingredients } from "./constants";
+import { hashSync } from 'bcrypt';
+import { prisma } from './prisma-client';
+import { categories, ingredients, products } from './constants';
+import { Prisma } from '@prisma/client';
+
+const randomDecimalNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) * 10 + min * 10) / 10;
+};
+
+const generateProductItem = ({
+  productId,
+  profileType,
+  size,
+}: {
+  productId: number;
+  profileType?: 1 | 2;
+  size?:
+    | 1.2
+    | 1.25
+    | 1.3
+    | 1.35
+    | 1.4
+    | 1.45
+    | 1.5
+    | 1.8
+    | 2.0
+    | 2.5
+    | 3.0
+    | 4.0
+    | 4.5
+    | 5.0
+    | 5.5
+    | 6.0;
+}) => {
+  return {
+    productId,
+    price: randomDecimalNumber(190, 600),
+    profileType,
+    size,
+  } as Prisma.ProductItemUncheckedCreateInput;
+};
 
 async function up() {
   await prisma.user.createMany({
     data: [
       {
-        fullName: "User Test",
-        email: "user@test.ru",
-        password: hashSync("1111", 10),
+        fullName: 'User Test',
+        email: 'user@test.ru',
+        password: hashSync('1111', 10),
         verified: new Date(),
-        role: "USER",
+        role: 'USER',
       },
       {
-        fullName: "Admin Admin",
-        email: "admin@test.ru",
-        password: hashSync("1111", 10),
+        fullName: 'Admin Admin',
+        email: 'admin@test.ru',
+        password: hashSync('1111', 10),
         verified: new Date(),
-        role: "ADMIN",
+        role: 'ADMIN',
       },
     ],
   });
@@ -28,12 +66,131 @@ async function up() {
   await prisma.ingredient.createMany({
     data: ingredients,
   });
+  await prisma.product.createMany({
+    data: products,
+  });
+
+  const profile1 = await prisma.product.create({
+    data: {
+      name: 'REHAU 245536',
+      imageUrl: 'public/assets/REHAU 245536.png',
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(0, 7).map((i) => ({ id: i.id })),
+      },
+    },
+  });
+  const profile2 = await prisma.product.create({
+    data: {
+      name: 'Труба сварная 40х50х2мм (6м)',
+      imageUrl: 'public/assets/труба-сварная-40х50х2мм-6м.jpg',
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(7, 13).map((i) => ({ id: i.id })),
+      },
+    },
+  });
+  const profile3 = await prisma.product.create({
+    data: {
+      name: 'Полоса оцинкованная 100х6мм (6м)',
+      imageUrl: 'public/assets/Полоса оцинкованная 100х6мм (6м).jpg',
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(14, 21).map((i) => ({ id: i.id })),
+      },
+    },
+  });
+
+  await prisma.productItem.createMany({
+    data: [
+      // Профиль REHAU 245536
+      generateProductItem({
+        productId: profile1.id,
+        profileType: 1,
+        size: 1.2,
+      }),
+      generateProductItem({
+        productId: profile1.id,
+        profileType: 1,
+        size: 1.45,
+      }),
+      generateProductItem({
+        productId: profile1.id,
+        profileType: 1,
+        size: 1.8,
+      }),
+
+      generateProductItem({
+        productId: profile1.id,
+        profileType: 2,
+        size: 1.35,
+      }),
+      generateProductItem({
+        productId: profile1.id,
+        profileType: 2,
+        size: 1.5,
+      }),
+      generateProductItem({
+        productId: profile1.id,
+        profileType: 2,
+        size: 1.8,
+      }),
+      generateProductItem({
+        productId: profile1.id,
+        profileType: 2,
+        size: 2.0,
+      }),
+
+      // Труба сварная 40х50х2мм (6м)
+      generateProductItem({
+        productId: profile2.id,
+        profileType: 2,
+        size: 1.8,
+      }),
+      generateProductItem({
+        productId: profile2.id,
+        profileType: 2,
+        size: 2.0,
+      }),
+
+      // Полоса оцинкованная 100х6мм (6м)
+      generateProductItem({
+        productId: profile3.id,
+
+        size: 5.5,
+      }),
+      generateProductItem({
+        productId: profile3.id,
+
+        size: 6.0,
+      }),
+
+      // Остальные продукты
+      generateProductItem({ productId: 1 }),
+      generateProductItem({ productId: 2 }),
+      generateProductItem({ productId: 3 }),
+      generateProductItem({ productId: 4 }),
+      generateProductItem({ productId: 5 }),
+      generateProductItem({ productId: 6 }),
+      generateProductItem({ productId: 7 }),
+      generateProductItem({ productId: 8 }),
+      generateProductItem({ productId: 9 }),
+      generateProductItem({ productId: 10 }),
+      generateProductItem({ productId: 11 }),
+      generateProductItem({ productId: 12 }),
+      generateProductItem({ productId: 13 }),
+      generateProductItem({ productId: 14 }),
+      generateProductItem({ productId: 15 }),
+      generateProductItem({ productId: 16 }),
+      generateProductItem({ productId: 17 }),
+    ],
+  });
 }
 
 async function down() {
-  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
-  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
-  await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRawUnsafe(
+    `TRUNCATE TABLE "ProductItem", "Product", "Ingredient", "Category", "User" RESTART IDENTITY CASCADE`
+  );
 }
 
 async function main() {
