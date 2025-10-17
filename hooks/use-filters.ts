152f3cd8eca6c601f3ae2b0f3ff6e76ee2ec11ca
Victor <linesofcode404@gmail.com>
2025-10-17 11:37:31 +0300

    fix(filters): убрано зацикливание при изменении цены
    - обновлён компонент Filters
    - исправлен useQueryFilters: добавлена проверка на совпадение URL
    - синхронизация query через useSearchParams

--- hooks/use-filters.ts
+++ hooks/use-filters.ts
@@ -1,87 +1,67 @@
+"use client";
 
-import {  useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { useSet } from "react-use";
 import React from "react";
-
 
 interface PriceProps {
   priceFrom?: number;
   priceTo?: number;
-  onChangePriceFrom?: (value: number) => void;
-  onChangePriceTo?: (value: number) => void;
-  className?: string;
 }
 
- interface QueryFilters extends PriceProps {
-  ingredients: string;
-  sizes: string;
-  profileType: string;
-}
- export interface Filters {
+export interface Filters {
   sizes: Set<string>;
   materialsTypes: Set<string>;
   selectedIngredients: Set<string>;
   prices: PriceProps;
-
-  // setPrices: (name: keyof PriceProps, value: number) => void;
-  // setMaterialsTypes: (value: string) => void;
-  // setSizes: (value: string) => void;
-  // setIngredients: (value: string) => void;
 }
 
 interface ReturnProps extends Filters {
- setPrices: (name: keyof PriceProps, value: number) => void;
- setMaterialsTypes: (value: string) => void;
- setSizes: (value: string) => void;
- setSelectedIngredients: (value: string) => void;
-  
+  setPrices: (name: keyof PriceProps, value: number) => void;
+  setMaterialsTypes: (value: string) => void;
+  setSizes: (value: string) => void;
+  setSelectedIngredients: (value: string) => void;
 }
+
 export const useFilters = (): ReturnProps => {
- 
-    const searchParams = useSearchParams() as unknown as Map<
-        keyof QueryFilters,
-        string
-      >;
-/* фильтрация ингредиентов */
-       const [selectedIngredients, { toggle: toggleIngredients }] = useSet(
-       new Set(searchParams.get("ingredients")?.split(",") ?? []));
+  const searchParams = useSearchParams();
 
-  
-/* фильтрация размеров */
-   const [sizes, { toggle: toggleSizes }] = useSet(
-  new Set(searchParams.get("sizes")?.split(",") ?? [])
-);
-/* фильтрация типов профиля */
-    const [materialsTypes, { toggle: toggleMaterialsTypes }] = useSet(
-  new Set(searchParams.get("profileType")?.split(",") ?? [])
-);
-/* фильтрация по цене */
-      const [prices, setPrices] = React.useState<PriceProps>(() => ({
-  priceFrom: searchParams.get("priceFrom")
-    ? Number(searchParams.get("priceFrom"))
-    : undefined,
-  priceTo: searchParams.get("priceTo")
-    ? Number(searchParams.get("priceTo"))
-    : undefined,
-}));
+  const [selectedIngredients, { toggle: toggleIngredients }] = useSet(
+    new Set(searchParams.get("ingredients")?.split(",") ?? [])
+  );
 
-   const updatePrice = (name: keyof PriceProps, value: number) => {
-      setPrices((prev) => ({
-        ...prev,
-        [name]: value,
-      }));
-    };
+  const [sizes, { toggle: toggleSizes }] = useSet(
+    new Set(searchParams.get("sizes")?.split(",") ?? [])
+  );
 
-    
+  const [materialsTypes, { toggle: toggleMaterialsTypes }] = useSet(
+    new Set(searchParams.get("profileType")?.split(",") ?? [])
+  );
+
+  const [prices, setPrices] = React.useState<PriceProps>(() => ({
+    priceFrom: searchParams.get("priceFrom")
+      ? Number(searchParams.get("priceFrom"))
+      : undefined,
+    priceTo: searchParams.get("priceTo")
+      ? Number(searchParams.get("priceTo"))
+      : undefined,
+  }));
+
+  const updatePrice = (name: keyof PriceProps, value: number) => {
+    setPrices((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   return {
     sizes,
     materialsTypes,
     selectedIngredients,
     prices,
-    setPrices: updatePrice ,
+    setPrices: updatePrice,
     setMaterialsTypes: toggleMaterialsTypes,
     setSizes: toggleSizes,
-    setSelectedIngredients: toggleIngredients
-  }
+    setSelectedIngredients: toggleIngredients,
+  };
 };
--- hooks/use-query-filters.ts
+++ hooks/use-query-filters.ts
@@ -1,27 +1,31 @@
+"use client";
 
 import React from "react";
 import qs from "qs";
 import { Filters } from "./use-filters";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export const useQueryFilters = (filters: Filters) => {
-    const router = useRouter();
-      React.useEffect(() => {
-  const params = {
-    ...filters.prices,
-    sizes: Array.from(filters.sizes),
-    materialsTypes: Array.from(filters.materialsTypes),
-    ingredients: Array.from(filters.selectedIngredients),
-  };
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentQuery = searchParams.toString(); 
 
-  const query = qs.stringify(params, {
-    skipNulls: true,
-    arrayFormat: "comma",
-  });
+  React.useEffect(() => {
+    const params = {
+      ...filters.prices,
+      sizes: Array.from(filters.sizes),
+      materialsTypes: Array.from(filters.materialsTypes),
+      ingredients: Array.from(filters.selectedIngredients),
+    };
 
-  const newUrl = `?${query}`;
-  if (newUrl !== window.location.search) {
-    router.replace(newUrl, { scroll: false });
-  }
-}, [filters, router]);
+    const query = qs.stringify(params, {
+      skipNulls: true,
+      arrayFormat: "comma",
+    });
+
+    const newUrl = `?${query}`;
+    if (newUrl !== `?${currentQuery}`) {
+      router.replace(newUrl, { scroll: false });
+    }
+  }, [filters, router, currentQuery]);
 };
