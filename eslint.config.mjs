5557bd96279512954400cc4be16ae52c04aa0196
Victor <linesofcode404@gmail.com>
2025-10-17 17:40:53 +0300

    chore: настроен Prettier и обновлены конфиги ESLint/VSCode

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "@next/next/no-img-element": "off",
+      "@next/next/no-img-element": "off",
+
+    },
+  },
+
 ];
 
 export default eslintConfig;
