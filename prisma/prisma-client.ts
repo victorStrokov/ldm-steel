058f0f528c715ec65407e1c6ea03f80ce9176fc2
Victor <144254586+victorStrokov@users.noreply.github.com>
2026-02-19 00:10:12 +0300

    fix: use standard Prisma client

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -1,17 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
-
-const adapter = new PrismaPg(pool);
-
-const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter });
-};
+const prismaClientSingleton = () => new PrismaClient();
 
 declare global {
   var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
