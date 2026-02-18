9e617562a85607b64bc51a0d912662cf131624d8
Victor <144254586+victorStrokov@users.noreply.github.com>
2026-02-19 00:28:14 +0300

    fix: pass datasourceUrl to PrismaClient

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
-const prismaClientSingleton = () => new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient({ datasourceUrl: process.env.DATABASE_URL });
+};
 
 declare global {
   var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
