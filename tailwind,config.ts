63de82a294ca922c554e4630835bb60727ddd201
Victor <144254586+victorStrokov@users.noreply.github.com>
2026-03-26 21:55:25 +0300

    tailwind4

---
+++ tailwind,config.ts
@@ -0,0 +1,7 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./app/**/*.{ts,tsx}', './shared/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+};
+
+export default config;
